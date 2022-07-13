01ac9e7bda44b6d77cdd180abd92800f5bb184d0
Martin Průša <73097610+Martin-Prusa@users.noreply.github.com>
2022-07-13 13:17:31 +0200

    licence input

--- src/app/components/riders/rider-new/rider-new.component.ts
+++ src/app/components/riders/rider-new/rider-new.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-rider-new',
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RiderNewComponent implements OnInit {
 
-  constructor() { }
+  numbers: number[] = Array(11)
+
+  constructor() {}
 
   ngOnInit(): void {
+  }
+
+  nextNum(e: KeyboardEvent, prev: HTMLInputElement | null, current: HTMLInputElement, next: HTMLInputElement | null) {
+    if(current.value.length > 1) current.value = current.value.substring(0,1)
+    if(e.key === 'Backspace') {
+      if(prev) {
+        prev.value = ''
+        prev.focus()
+      }
+      return
+    }
+    
+    if(!isNaN(parseInt(current.value))) {
+      if(next) return next.focus()
+    } 
+    else current.value = ''
+  }
+
+  onPaste(e: any, last: HTMLInputElement) {
+    let paste = (e.clipboardData)
+          .getData('text')
+          .trim()
+          .split('')
+          .map((i: string) => parseInt(i))
+          .filter((i: number) => !isNaN(i))
+    if(paste.length === 11) {
+      this.numbers = paste
+      last.focus()
+    }
+   
   }
 
 }
