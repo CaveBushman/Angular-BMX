93d6c5b60a77c7f1598fd6316a6d3b350c0055bc
Martin Průša <73097610+Martin-Prusa@users.noreply.github.com>
2022-07-17 19:25:08 +0200

    userService start

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,13 +1,31 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, OnDestroy } from '@angular/core';
+import { tap } from "rxjs/operators";
+import { BehaviorSubject } from 'rxjs';
+import { IUser } from '../models/iuser';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UsersService {
+export class UsersService implements OnDestroy{
+
+  private URL = 'http://localhost:3000/api/v1/users/';
+
+  private _user = new BehaviorSubject<IUser | null>(null)
 
   isUserLogin: boolean = true
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  ngOnDestroy(): void {
+  }
+
+  login(email: string, password: string) {
+    const body = { email, password }
+    return this.http.post<any>(this.URL+'login', body).pipe(tap(data => {
+
+    }))
+  }
 
   getUserLogin() {
     return this.isUserLogin
