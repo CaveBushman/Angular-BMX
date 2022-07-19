c7133d7d1c63c7592fc399db222bdc4d75d3d433
CaveBushman <david@black-ops.eu>
2022-07-19 17:43:32 +0200

    user roles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,7 @@ import { RiderNew3Component } from './components/riders/rider-new3/rider-new3.co
 import { RegistrationsComponent } from './components/events/registrations/registrations.component';
 import { EventAdminComponent } from './components/events/event-admin/event-admin.component';
 import { LoginComponent } from './components/auth/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
@@ -25,23 +26,28 @@ const routes: Routes = [
   { path: 'events', component: EventsComponent },
   { path: `events/:id`, component: EventDetailComponent },
   { path: 'events/entry/:id', component: RegistrationsComponent },
-  { path: `events/admin/:id`, component: EventAdminComponent},
+  {
+    path: `events/admin/:id`,
+    component: EventAdminComponent,
+    canActivate: [AuthGuard],
+    data: { role: 'admin' },
+  },
   { path: 'riders', component: RidersComponent },
   { path: 'riders/:id', component: RiderDetailComponent },
   { path: 'rider-new', component: RiderNewComponent },
-  { path: `rider-new-3`, component: RiderNew3Component},
-  { path: `riders-admin`, component: RidersAdminComponent},
+  { path: `rider-new-3`, component: RiderNew3Component },
+  { path: `riders-admin`, component: RidersAdminComponent },
   { path: 'clubs', component: ClubsComponent },
   { path: `clubs/:id`, component: ClubDetailComponent },
   { path: 'rankings', component: RankingsComponent },
   { path: 'news', component: NewsComponent },
   { path: `news/:id`, component: NewsDetailComponent },
   { path: 'rules', component: RulesComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/models/iuser.ts
+++ src/app/models/iuser.ts
@@ -4,11 +4,7 @@ export interface IUser {
   firstName: string,
   lastName: string,
   email: string,
-<<<<<<< HEAD
   photo: string,
-
-=======
   role: string,
-  
->>>>>>> a33b9d4bc086fde62e340d9a67833c2fc61aa530
+
 }
