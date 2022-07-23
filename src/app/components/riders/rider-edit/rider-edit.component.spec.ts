23c1ca72d22b7615880ac0a796f024397e619e27
Martin Průša <martinprusa@Martin-iMac-24.localdomain>
2022-07-23 13:44:04 +0200

    edit rider

---
+++ src/app/components/riders/rider-edit/rider-edit.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RiderEditComponent } from './rider-edit.component';
+
+describe('RiderEditComponent', () => {
+  let component: RiderEditComponent;
+  let fixture: ComponentFixture<RiderEditComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RiderEditComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RiderEditComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
